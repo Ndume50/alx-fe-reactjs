fa4d090f883042f0644bccac976d08bf93f9db88
Dumisani Ndume <ndumedumisani50@gmail.com>
2025-08-31 23:08:28 +0200

    Add files via upload

--- alx-react-app/App.jsx
+++ alx-react-app/App.jsx
@@ -2,24 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
-     <h1>hello world</h1>
-    </>
-  )
+function WelcomeMessage() {
+    return ( 
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+        
+    );
+} <WelcomeMessage /> 
+
+
+   
+    
+  
+}
+
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Welcome to ALX React APP!</h1>
+            <p>This is a simple JSX component.</p>
+        </div>
+    );
 }
 
 export default App
